b0c5525d55087aa96de66881f84797f03170965b
NathanaelLC <160418673+NathanaelLC@users.noreply.github.com>
2025-10-19 08:57:21 +0700

    Update PingPongGame.py
    Adding class game sprite and tennis ball

--- PingPongGame.py
+++ PingPongGame.py
@@ -1,7 +1,16 @@
-import time
-
 from pygame import*
 '''Required classes'''
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed, width, height):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (width, height))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
 
 back = (200, 255, 255)
 win_width = 600
@@ -14,11 +23,15 @@ finish = False
 clock = time.Clock()
 FPS = 60
 
+ball = GameSprite('tennis_ball.jpg', 200, 200, 4, 50, 50)
+
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
-    window.fill(back)
+    if finish != True:
+        window.fill(back)
+        ball.reset()
     display.update()
     clock.tick(FPS)
 
